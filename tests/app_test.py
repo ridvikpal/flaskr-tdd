9a8c3a9d57d240d4217bfb0359e9bbe77a43d150
Ridvik Pal <ridvikpal@gmail.com>
2025-09-27 22:27:29 -0400

    Minor update to ordering in app_test.py.

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,9 +1,9 @@
-import os
-import pytest
 import json
 from pathlib import Path
-from project.app import app, db
 
+import pytest
+
+from project.app import app, db
 
 TEST_DB = "test.db"
 
@@ -75,6 +75,7 @@ def test_messages(client):
     assert b"No entries here so far" not in rv.data
     assert b"&lt;Hello&gt;" in rv.data
     assert b"<strong>HTML</strong> allowed here" in rv.data
+
 
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
